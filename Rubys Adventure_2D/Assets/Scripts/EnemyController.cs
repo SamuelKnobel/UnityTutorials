cf8fa15cb8c716d31ff04f32252e45f499d29a6e
samuelknobel <samuelknobel@hotmail.com>
2020-05-04 11:28:50 +0200

    add other Tutorials Rubys Adventure

---
+++ Rubys Adventure_2D/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed = 3.0f;
+    public bool vertical;
+    public float changeTime = 3.0f;
+
+    public ParticleSystem smokeEffect;
+
+    Rigidbody2D rb2d;
+    float timer;
+    int direction = 1;
+    Animator animator;
+
+    bool broken;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        broken = true;
+        rb2d = GetComponent<Rigidbody2D>();
+        timer = changeTime;
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //remember ! inverse the test, so if broken is true !broken will be false and return won’t be executed.
+        if (!broken)
+        {
+            return;
+        }
+        timer -= Time.deltaTime;
+
+        if (timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+        }
+
+
+        Vector2 position = rb2d.position;
+
+        if (vertical)
+        {
+            animator.SetFloat("MoveX", 0);
+            animator.SetFloat("MoveY", direction);
+            position.y = position.y + Time.deltaTime * speed * direction;
+        }
+        else
+        {
+            animator.SetFloat("MoveX", direction);
+            animator.SetFloat("MoveY", 0);
+            position.x = position.x + Time.deltaTime * speed * direction;
+        }
+
+        rb2d.MovePosition(position);
+    }
+    public void Fix()
+    {
+        broken = false;
+        rb2d.simulated = false;
+        animator.SetTrigger("Fixed");
+        smokeEffect.Stop();
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        RubyController player = other.gameObject.GetComponent<RubyController>();
+
+        if (player != null)
+        {
+            player.ChangeHealth(-1);
+        }
+    }
+}
